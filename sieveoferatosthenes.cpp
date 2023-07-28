bb04e12c71c50da4c870143f34b94bf43ca90e81
pappla komal harshit <komalharshit2003@gmail.com>
2023-07-28 08:29:32 +0530

    Add files via upload

---
+++ sieveoferatosthenes.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void sieve(int n){
+
+
+vector<bool> isPrime(n+1,true);
+
+for (int i = 2; i*i < n; i++)
+{
+    if(isPrime[i]){
+    for (int j = i*i; j <=n; j+=i)
+    {
+       isPrime[j] = false;
+    }
+    }
+    
+}
+
+
+for (int i = 2; i <=n; i++)
+{
+    if(isPrime[i]){
+        cout << i<<" ";
+        
+    }
+}
+
+
+}
+int main(){
+
+int n;
+
+cout << "Enter the number range till which u want the list of primes : ";
+
+cin>>n;
+
+sieve(n);
+
+
+return 0;
+}
+
+
